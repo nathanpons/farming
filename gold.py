326c2fc69bac8b0286c3672fad3c52e65af1c4df
PapsBurr <npons3932@gmail.com>
2026-03-12 14:58:54 -0600

    feat: add maze path finding that utilizes drones to search each path

--- gold.py
+++ gold.py
@@ -29,6 +29,18 @@ def create_maze(start_x=0, start_y=0, grid_size=4):
             break
 
 
+def check_all_directions():
+
+    directions = [North, East, South, West]
+    open_paths = []
+
+    for direction in directions:
+        if can_move(direction):
+            open_paths.append(direction)
+
+    return open_paths
+
+
 def nav_maze_hug_right(prev_direction=North):
 
     right_of = {North: East, East: South, South: West, West: North}
@@ -56,6 +68,47 @@ def nav_maze_hug_right(prev_direction=North):
             continue
 
 
+def drone_nav_maze_hug_right(prev_direction=North):
+
+    def dnmhr():
+        right_of = {North: East, East: South, South: West, West: North}
+        left_of = {North: West, West: South, South: East, East: North}
+
+        # move once to get into the branch
+        move(prev_direction)
+
+        # Loop till we find the gold
+        while is_not_solved:
+
+            # Check tile for gold
+            if get_entity_type() == gold:
+                harvest()
+                is_not_solved = False
+                return
+
+            # Check for multiple branching paths and send a drone down each, then return
+            open_paths = check_all_directions()
+            if len(open_paths) > 1:
+                for direction in open_paths:
+                    spawn_drone(drone_nav_maze_hug_right(direction))
+                return
+
+            # Navigate maze by moving either Right->Forward->Backward->Left
+            if move(right_of[prev_direction]):
+                prev_direction = right_of[prev_direction]
+                continue
+            elif move(prev_direction):
+                continue
+            elif move(left_of[prev_direction]):
+                prev_direction = left_of[prev_direction]
+                continue
+            elif move(right_of[right_of[prev_direction]]):
+                prev_direction = right_of[right_of[prev_direction]]
+                continue
+
+    return dnmhr
+
+
 def farm_gold(start_x=0, start_y=0, grid_size=4):
     while True:
         create_maze(start_x, start_y, grid_size)
@@ -68,3 +121,15 @@ def drone_farm_gold(start_x=0, start_y=21, grid_size=11):
         farm_gold(start_x, start_y, grid_size)
 
     return dfg
+
+
+def drone_solve_maze(start_x=16, start_y=16):
+    create_maze(start_x, start_y)
+    global is_not_solved
+    is_not_solved = True
+
+    def dsm():
+        change_hat(Hats.Gold_Hat)
+        drone_nav_maze_hug_right()
+
+    return dsm
