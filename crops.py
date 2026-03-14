bb9848e9fa03a8b519a12cfc147aaec1d357b9fb
PapsBurr <npons3932@gmail.com>
2026-03-14 11:38:04 -0600

    feat: adds general crop useful functions like planting grids

---
+++ crops.py
@@ -0,0 +1,41 @@
+from __builtins__ import *
+import movement_utils
+
+
+def plant_grid(start_x=0, start_y=0, size_x=6, size_y=6, crop=Entities.Grass):
+
+    for x in range(size_x):
+        for y in range(size_y):
+            movement_utils.nav_to_tile(x + start_x, y + start_y)
+            if get_ground_type() != Grounds.Soil:
+                till()
+            if get_entity_type() != crop:
+                harvest()
+                plant(crop)
+
+
+def drone_plant_grid(start_x=0, start_y=0, size_x=6, size_y=6, crop=Entities.Grass):
+
+    def dpg():
+        for x in range(size_x):
+            for y in range(size_y):
+                movement_utils.nav_to_tile(x + start_x, y + start_y)
+                if get_ground_type() != Grounds.Soil:
+                    till()
+                if get_entity_type() != crop:
+                    harvest()
+                    plant(crop)
+
+    return dpg
+
+
+def plant_grid_drones(start_x=0, start_y=0, size_x=6, size_y=6, crop=Entities.Grass):
+
+    for x in range(size_x):
+        for y in range(size_y):
+            while True:
+                drone = spawn_drone(
+                    drone_plant_grid(start_x + x, start_y, 1, size_y, crop)
+                )
+                if drone:
+                    break
